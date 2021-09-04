e5021c639e2f1f32ad6066383b382bbf1623f378
schuub <4443553-schuub@users.noreply.gitlab.com>
2021-09-04 22:04:37 +0200

    [IMP] parsing to dict

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import re
 from bs4 import BeautifulSoup
 # import requests
 
 
 if __name__ == "__main__":
-    with open("ursina_cheat_sheet.html") as file:
+    with open("html/ursina_cheat_sheet.html") as file:
         soup = BeautifulSoup(file, 'html5lib')
 
     docs = dict()
@@ -11,16 +12,20 @@ if __name__ == "__main__":
     content = soup.find('div', id='content') \
         .find_all('div', recursive=False)
 
-    for e in content[48:50]:
+    for e in content[50:51]:
+        info = e.contents[1]
         search_string = e.div['id'].lower()
+        example = info.find('div', class_='example').extract()
+
+        regex = re.compile(r'(?:\n)([a-z_A-Z]+\(.*\))')
+        methods = regex.findall(info.text)
         res = {
             'title': e.div.text,
-            'github_url': e.contents[1].a['href'],
-            'example': "test",
+            'github_url': info.a['href'],
+            'example': example.text,
+            'params': info.find('params').text,
+            'methods': methods,
         }
-        print(search_string)
-        print(e.contents[1].a['href'])
-        print()
         docs[search_string] = res
 
-    print(docs['vec3']['github_url'])
+    print(docs['animation']['methods'])
